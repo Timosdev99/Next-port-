e070e9b92ffdbf55ee915c525ddf384f259ef750
timosdev99 <timothyisah4@gmail.com>
2024-12-14 01:20:29 +0100

    added post

--- src/app/users/route.jsx
+++ src/app/users/route.jsx
@@ -1,9 +1,28 @@
-export const user = [
+import { json } from "body-parser";
+
+export const users = [
     {id: 1, Name: "ben"},
-    {id: 1, Name: "ben"},
+    {id: 2, Name: "ben"},
 
 ]
 
 export const GET =  () => {
-    return Response.json(user)
+    return Response.json(users)
+}
+
+export const POST = async(request) => {
+    const user = await request.json();
+    const newuser = {
+        id: users.length + 1,
+        Name: user.name,
+    }
+users.push(newuser)
+return new Response(JSON.stringify(newuser), {
+    headers: {
+        "Content-Type": "application/json",
+
+    },
+    status: 201,
+
+})
 }
