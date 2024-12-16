24f6defbe68fe4abbde9daba368ecf88be6fc885
timosdev99 <timothyisah4@gmail.com>
2024-12-16 13:09:52 +0100

    added clerk auth

--- src/app/component/nav.jsx
+++ src/app/component/nav.jsx
@@ -1,14 +1,22 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 
 export const Nav = () => {
     const pathname = usePathname()
     return(
-        <nav>
+        <nav className="flex justify-center items-center p-4"> 
             <Link href="/" className={pathname === "/" ? "font-bold mr-4" : "mr-4 text-blue-500"}>Home</Link>
             <Link href="/about" className={pathname === "/about" ? "font-bold mr-4" : "mr-4 text-blue-500"}>about</Link>
             <Link href="/product" className={pathname === "/product/macbook" ? "font-bold mr-4" : "mr-4 text-blue-500"}>product</Link>
+          <SignedOut>
+          <SignInButton mode="modal"></SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+          <UserButton />
+          </SignedIn>
         </nav>
     )
 }
